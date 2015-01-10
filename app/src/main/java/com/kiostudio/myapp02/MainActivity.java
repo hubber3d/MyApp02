af1ba521632b18759034d3f9e9ba82203c90c006
hubber3d <hubber3d@gmail.com>
2015-01-10 16:47:11 +0700

    My commit message
    Some change 2-2

--- app/src/main/java/com/kiostudio/myapp02/MainActivity.java
+++ app/src/main/java/com/kiostudio/myapp02/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //some change 02
+    //some change 02-2
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
